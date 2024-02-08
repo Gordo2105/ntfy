6cd0d9cea2686aedbac801a5bae094f4eecb94a0
Gordo <159019957+Gordo2105@users.noreply.github.com>
2024-02-08 11:43:09 +0000

    Delete convhtml.sh

--- convhtml.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Converte ficheiros txt em html
-for file in *.txt; do
-    if [ -f "$file" ]; then
-        filename="${file%.*}"
-        pandoc -s "$file" -o "${filename}.html"
-    fi
-done
